c8290fa2645d447a8a9e387e96662c6188e19ae1
Blue Carn <blue.carn@student.manchester.ac.uk>
2025-12-16 18:18:36 +0000

    Create more complex baseline

--- axion_haloscope/external_noise.py
+++ axion_haloscope/external_noise.py
@@ -4,5 +4,5 @@ def external_noise(
     freqs_hz: np.ndarray, f_start_hz: float, f_range: float
 ) -> np.ndarray:
     x = (freqs_hz - f_start_hz)/f_range
-    y = 20.0*x + 1.0
+    y = 2.0*x**2 + 3*x + 12
     return y
