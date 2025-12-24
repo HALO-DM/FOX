b0aa0700c5bfe945fa69a6db202f2e84e4a9647c
Blue Carn <blue.carn@student.manchester.ac.uk>
2025-12-24 16:23:22 +0000

    Fix external_noise

--- axion_haloscope/external_noise.py
+++ axion_haloscope/external_noise.py
@@ -3,6 +3,12 @@ import numpy as np
 def external_noise(
     freqs_hz: np.ndarray, f_start_hz: float, f_range: float
 ) -> np.ndarray:
-    x = (freqs_hz - f_start_hz)/f_range
-    y = 2.0*x**2 + 3*x + 12
+    x = (freqs_hz - f_start_hz)
+
+    
+
+    y = -2.0*x**3 - 3*x**2 + 12*x + 5
+    #y = 8*x**7 - 7*x**6 + 13*x**5 + 16*x**4 - 20*x**3 + 5*x**2 + 11*x + 6
+    #y = np.sin(x) + 3
+    #y = 1
     return y
